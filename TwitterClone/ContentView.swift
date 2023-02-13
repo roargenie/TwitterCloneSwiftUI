3c8095186a4c269c5cf94212ec6a86f118869f8f
roargenie <lazyhoper987@gmail.com>
2023-02-13 20:56:17 +0900

    git init

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by 이명진 on 2023/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by 이명진 on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
